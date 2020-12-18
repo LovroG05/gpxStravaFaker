e50dc273bb9ac07bd282d3855334548ae7b7b3f2
LovroG05 <lovro.govekar@gmail.com>
2020-12-18 21:55:56 +0100

    1.0

---
+++ main.py
@@ -0,0 +1,54 @@
+import time, datetime
+import gpxpy
+import xml.etree.ElementTree as ET
+import os
+import sys
+import re
+import arrow
+
+
+def timedeltaget(time1, time2):
+    timed = arrow.get(time1) - arrow.get(time2)
+    return timed
+
+
+def getnewdate(old_date, timedelta):
+    old_date = arrow.get(old_date).datetime
+    new_date = old_date + timedelta
+    new_date = str(new_date)
+    new_date = re.sub(" ", "T", new_date)
+    new_date = re.sub("\+00:00", "Z", new_date)
+    return new_date
+
+
+# defs
+ET.register_namespace('', "http://www.topografix.com/GPX/1/1")
+ET.register_namespace("", "http://www.topografix.com/GPX/1/1/gpx.xsd")
+ET.register_namespace("", "http://www.w3.org/2001/XMLSchema-instance")
+ET.register_namespace("", "http://www.topografix.com/GPX/1/1")
+
+# inputs
+tree = ET.parse(input("path>> "))
+root = tree.getroot()
+timenew = input("time[YYYY-MM-DDThh:mm:ssZ]>> ")
+
+# get timedelta
+_timedelta = timedeltaget(root[0][0].text, timenew)
+print(_timedelta)
+
+# change initial time of gpx
+root[0][0].text = getnewdate(root[0][0].text, _timedelta)
+
+
+# https://arrow.readthedocs.io/en/stable/
+# change time and date in gps recordings
+for child in root[1][2]:
+    # print(child[1].tag, child[1].text)
+    # change date and time
+    new_date_gpx = getnewdate(child[1].text, _timedelta)
+    # write date and time to gpx
+    child[1].text = new_date_gpx
+    print(new_date_gpx)
+
+# save the file
+tree.write("newactivity.gpx")
